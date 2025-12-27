4e22eeb2dfdf8b9c4d94df69a74ffb9b0cd2fc8b
Johan Castaño <johan.castano@lilab.pe>
2025-12-27 11:37:01 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'calidad-cloud-test-ionic',
+  webDir: 'www'
+};
+
+export default config;
